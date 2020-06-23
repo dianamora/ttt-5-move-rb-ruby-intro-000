3143b7301ba14b30fdde41880781bb339844dbb3
morra.dianna <morra.dianna@gmail.com>
2020-06-23 01:22:22 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+#1. Define a method to convert a user's input to an array index.
+#2. Define a method that updates an array passed to it.
+#3. Define a method with a default value.
+#4. Use a method in a CLI.
+#5. Accept user input via gets.
+#6. Use user input in a method.
+
+def input_to_index
+  
+end
+
+def move 
+  
+end 
